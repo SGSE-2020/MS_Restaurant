850182d63abcff174f7de5e3e54892b8b227cac3
akirsch1 <andre.kirsch@fh-bielefeld.de>
2020-05-20 17:29:25 +0200

    Added docker-compose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
-const path = require('path');
-const mali = require('mali');
+const path = require('path')
+const mali = require('mali')
 const express = require('express')
 
+const REST_PORT = 8081
+
 function main() {
-    //const gRpcServer = new mali(path.resolve(__dirname, '../proto/restaurants.proto'), 'RestaurantsService');
+    const grpc = new mali(path.resolve(__dirname, './proto/restaurants.proto'), 'RestaurantsService')
+    const rest = express()
+
+    grpc.start('0.0.0.0:50051')
+    rest.listen(REST_PORT, () => {
+        console.log('Rest Server started on port ' + REST_PORT)
+    })
 }
 
 main()
