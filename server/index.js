0d7eda6d2a2ab7b35be45ba87ae0d6af58f5fe86
akirsch1 <akirsch1@fh-bielefeld.de>
2020-05-20 16:01:39 +0200

    setup backend

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const mali = require('mali');
+const express = require('express')
+
+function main() {
+    //const gRpcServer = new mali(path.resolve(__dirname, '../proto/restaurants.proto'), 'RestaurantsService');
+}
+
+main()
