0dd557234a97e6f1f87f34d87206d798635f4a78
akirsch1 <andre.kirsch@fh-bielefeld.de>
2020-05-26 00:29:33 +0200

    Started creation of frontend

---
+++ client/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav id="restaurants_nav" class="level">
+        <div class="level-left">
+            <img>
+            <h1>Smart City - Restaurants</h1>
+        </div>
+        <div class="level-right">
+            <b-button type="is-success" size="is-medium">Login</b-button>
+        </div>
+    </nav>
+</template>
+
+<style>
+#restaurants_nav {
+  padding: 16px 16px;
+  background-color: #2c3e50;
+}
+
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #42b983;
+  font-weight: 900;
+  font-size: xx-large;
+}
+</style>
